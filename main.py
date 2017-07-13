da261cd553082fb9bd0611a1e86b07e17c1afcb5
Franklin <franklid@ds.ad.adp.com>
2017-07-12 18:37:39 -0700

    added login and signup html files, and their handlers.

--- main.py
+++ main.py
@@ -14,13 +14,25 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
     submitted = db.Column(db.Boolean)
+    owner_id =  db.Column(db.Integer, db.ForeignKey('user.id'))
     
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         self.submitted = False
+        self.owner = owner
 
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
 @app.route('/', methods=['GET'])
@@ -52,6 +64,7 @@ def addBlogEntry():
     if request.method == 'POST':
         blog_name_title = request.form['blog_title']
         blog_name_body = request.form['blog_body']
+        #blog_name_owner = request.form['blog_owner'] - create this in addBlogEntry
         if len(blog_name_title) == 0:
             title_error = 'Please enter a title'
 
@@ -59,7 +72,7 @@ def addBlogEntry():
             blog_error = 'Please write your blog'
 
         if not title_error and not blog_error:
-            new_blog = Blog(blog_name_title, blog_name_body)
+            new_blog = Blog(blog_name_title, blog_name_body, blog_name_owner)#blog_name_owner not connected to form on main.py or html yet
             db.session.add(new_blog)
             db.session.commit()     
            
@@ -77,3 +90,43 @@ def singleBlogs():
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user and password:
+            session['username'] = username
+            flash("Logged in")
+            return redirect('/')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+            
+    return render_template('login.html')
+
+@app.route('/signUp')
+def signUp():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        existing_user = User.query.filter_by(username=username).first()
+        
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/')
+        else:
+            
+            return "<h1>Duplicate User</h1>"
+
+    return render_template('signup.html')
+
