a4219060304948f050b941aed0f7e1ff5f872116
Franklin <franklid@ds.ad.adp.com>
2017-07-15 15:47:12 -0700

    changed sql database

--- blogz/main.py
+++ blogz/main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'super_secret_key'
